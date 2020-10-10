df96c63074aeb06f096bdcec5aff8701b3a98682
Viktor Ödman <vo222dq@student.lnu.se>
2020-10-10 13:51:18 +0200

    Trying to fix filepath

--- a2/index.php
+++ a2/index.php
@@ -8,7 +8,9 @@ require_once('./Settings.php');
 require_once('controller/LoginApp.php');
 /* require_once('../common/loginModule/src/Authenticator.php'); */
 /* require_once(dirname(__FILE__) . '/../common/loginModule/src/Authenticator.php'); */
-require_once($_SERVER['DOCUMENT_ROOT'] . '/common/loginModule/src/Authenticator.php');
+require_once(__DIR__ . '/../common/loginModule/src/Authenticator.php' );
+
+var_dump( realpath( __DIR__ . '/../common/loginModule/src/Authenticator.php' ) );
 
 
 
