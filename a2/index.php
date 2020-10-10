6a0e27536b77ced2a554bf8e52fe18764850a223
Viktor Ödman <vo222dq@student.lnu.se>
2020-10-10 12:58:59 +0200

    Fixing filepath

--- a2/index.php
+++ a2/index.php
@@ -7,7 +7,7 @@ session_start();
 require_once('./Settings.php');
 require_once('controller/LoginApp.php');
 /* require_once('../common/loginModule/src/Authenticator.php'); */
-require_once(__DIR__ . '/../common/loginModule/src/Authenticator.php');
+require_once(dirname(__FILE__) . '/../common/loginModule/src/Authenticator.php');
 
 $settings = new \Settings();
 
